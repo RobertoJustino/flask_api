78cfc99815c2197de278d8161fedcaa0d33c257f
Roberto Justino <60917775+RobertoJustino@users.noreply.github.com>
2021-12-02 10:56:58 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from flask import Flask, render_template, jsonify, url_for
+import os
+from flask import Flask, render_template, jsonify, url_for, json
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return render_template('layout.html')
+
+with open('./books.json', 'r') as myfile:
+    data = myfile.read()
+
+@app.route("/booksJSON")
+def booksJSON():
+    return render_template('books.html', data=jsonify(data))
           
 @app.route('/api/books')
 def books():
     return jsonify(book)
 
-@app.route('/api/books/<int:id>')
-def book(id):
-    return ""
+@app.route("/api/book/<int:id>", methods=["GET"])
+def get_book(id):
+    book = None
+    #boucle et recupere le isbn==id
+    for i in book:
+        for j, value in i:
+            if value == id:
+                book == i
+    return jsonify(book)
         
 
 @app.route('/urls')
 def urls():
     return "".format(url_for("books"))
+
 
 book=[
 	{
