d845d91d8d34b8c09a36967e82a2441a08bdbb6d
Roberto Justino <60917775+RobertoJustino@users.noreply.github.com>
2021-12-03 09:07:52 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,30 +2,30 @@ import os
 from flask import Flask, render_template, jsonify, url_for, json
 app = Flask(__name__)
 
+data = json.load(open("./books.json"))
+
 @app.route("/")
 def index():
     return render_template('layout.html')
 
-with open('./books.json', 'r') as myfile:
-    data = myfile.read()
 
 @app.route("/booksJSON")
 def booksJSON():
-    return render_template('books.html', data=jsonify(data))
+	return render_template('books.html', data=json.dumps(data))
           
 @app.route('/api/books')
 def books():
-    return jsonify(book)
+    return jsonify(books)
 
 @app.route("/api/book/<int:id>", methods=["GET"])
 def get_book(id):
-    book = None
-    #boucle et recupere le isbn==id
-    for i in book:
-        for j, value in i:
-            if value == id:
-                book == i
-    return jsonify(book)
+	book = None
+	#boucle et recupere le isbn==id
+	for val in data:
+		if val.get("isbn") == str(id):
+			book = val
+			break
+	return jsonify(book)
         
 
 @app.route('/urls')
@@ -33,7 +33,7 @@ def urls():
     return "".format(url_for("books"))
 
 
-book=[
+books=[
 	{
 		'id':1,
 		'titre' : 'un titre',
@@ -46,4 +46,4 @@ book=[
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
